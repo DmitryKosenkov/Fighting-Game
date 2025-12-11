5b1c64709dd4f8576336391e29a499d994e30c05
DmitryKosenkov <dmitrykosenkov.s@gmail.com>
2025-12-10 16:01:28 -0800

    Made the code cleaner
    Added folders for images and for JS

--- index.js
+++ index.js
@@ -7,66 +7,19 @@ canvas.height = 576
 c.fillRect(0, 0, canvas.width, canvas.height)
 
 const gravity = 0.4
+const speed = 10
+const flor = 58
 
-class Sprite{
-    constructor({position, velocity, color = 'red', offset}){
-        this.position = position
-        this.velocity = velocity
-        this.height = 100
-        this.width = 50
-        this.lastKey
-        this.attackBox = {
-            position:{
-                x: this.position.x,
-                y: this.position.y
-            },
-            offset,
-            width: 100,
-            height: 40
-        }
-        this.color = color
-        this.isAttacking
-        this.health = 100
-    }
+const background = new Sprite({
+    position:{
+        x: 0,
+        y: 0
+    },
+    imageSrc: './img/back/background3.png',
+    scale: 4
+})
 
-    draw(){
-        c.fillStyle = this.color
-        c.fillRect(this.position.x, this.position.y, this.width, this.height)
-    }
-
-    update(){
-        this.draw()
-
-        this.attackBox.position.x = this.position.x + this.attackBox.offset.x
-        this.attackBox.position.y = this.position.y
-        
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-
-        // attack box
-        if (this.isAttacking){
-            c.fillStyle = 'green'
-            c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.width, this.attackBox.height)
-        }
-        
-
-        if (this.position.y + this.height + this.velocity.y >= canvas.height){
-            this.velocity.y = 0
-        }
-        else{
-            this.velocity.y += gravity
-        }
-    }
-
-    attack(){
-        this.isAttacking = true
-        setTimeout(() => {
-            this.isAttacking = false
-        }, 100)
-    }
-}
-
-const player = new Sprite({
+const player = new Fighter({
     position:{
         x:0,
         y:0
@@ -83,7 +36,7 @@ const player = new Sprite({
 })
 
 
-const enemy = new Sprite({
+const enemy = new Fighter({
     position:{
         x:400,
         y:100
@@ -113,52 +66,15 @@ const keys = {
     ArrowLeft:{
         pressed: false
     },
-
 }
 
-function rectangualCollision({rectangle1, rectangle2}){
-    return(
-        rectangle1.attackBox.position.x + rectangle1.attackBox.width >= rectangle2.position.x &&
-        rectangle1.attackBox.position.x <= rectangle2.position.x + rectangle2.width &&
-        rectangle1.attackBox.position.y + rectangle1.attackBox.height >= rectangle2.position.y &&
-        rectangle1.attackBox.position.y <= rectangle2.position.y + rectangle2.height)
-}
-
-function determineWiner({player, enemy, timerId}){
-    clearTimeout(timerId)
-    document.querySelector("#displayText").style.display = 'flex'
-    if (player.health == enemy.health){
-        document.querySelector("#displayText").innerHTML = 'Draw'
-    }
-    else if (player.health > enemy.health){
-        document.querySelector("#displayText").innerHTML = 'Player 1 wins'
-    }
-    else if (enemy.health > player.health){
-        document.querySelector("#displayText").innerHTML = 'Player 2 wins'
-    }
-}
-
-
-let timer = 10
-let timerId
-function decreaseTimer(){
-    if (timer > 0){
-        timerId = setTimeout(decreaseTimer, 1000)
-        timer--
-        document.querySelector("#timer").innerHTML = timer
-    }
-    if (timer == 0){
-   
-        determineWiner({player, enemy, timerId})
-    }
-    
-}
 decreaseTimer()
 
 function animate(){
     window.requestAnimationFrame(animate)
     c.fillStyle = 'black'
     c.fillRect(0, 0 ,canvas.width, canvas.height)
+    background.update()
     player.update()
     enemy.update()
 
@@ -167,18 +83,18 @@ function animate(){
 
     // player movement
     if (keys.a.pressed && player.lastKey == 'a'){
-        player.velocity.x = -5
+        player.velocity.x = -speed
     }
     else if(keys.d.pressed && player.lastKey == 'd'){
-        player.velocity.x = 5
+        player.velocity.x = speed
     }
 
     // enemy movement
     if (keys.ArrowLeft.pressed && enemy.lastKey == 'ArrowLeft'){
-        enemy.velocity.x = -5
+        enemy.velocity.x = -speed
     }
     else if(keys.ArrowRight.pressed && enemy.lastKey == 'ArrowRight'){
-        enemy.velocity.x = 5
+        enemy.velocity.x = speed
     }
 
     // detect for collision
